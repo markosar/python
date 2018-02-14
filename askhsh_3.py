ab2c0ecd8aa725ac8151935eba60e150c1265d88
Markos Arakynthios <markos_ar@hotmail.com>
2018-02-14 16:59:37 +0200

    I am working on the calendar

--- askhsh_3.py
+++ askhsh_3.py
@@ -1,14 +1,14 @@
 keimeno = input("give me some text: ")
 keimeno = keimeno.split()## xwrizw to keimeno se lexeis
 P =""##arxikopoihsh kainourgias protashs
-for lexi in keimeno:##gia kathe lexi 
+for lexi in keimeno:##gia kathe lexi
     l = []##dhmiourgw kenh lista
     for i in range(len(lexi)):
         ascii = ord(lexi[i])##h antistoixhsh tou kathe grammatos ston kwdika ascii tou
         if(ascii >= 65 and ascii <= 90):##an einai kefalaio gramma
             if(ascii<= 77):##an to gramma einai mexri kai to gramma M
                 ascii+=13##kanw + 13 sto antistoixo ascii
-            else:##an einai apo to N kai meta 
+            else:##an einai apo to N kai meta
                 apostash_Z = 90 - ascii##ypologizw thn apostash tou ascii tou apo to ascii tou Z(90)
                 diafora = 13 - apostash_Z##afairw auth th apostash apo to 13 kai thetw to ascii tou xarakthra
                 ascii = 64 + diafora##iso me to athroisma tou ascii tou prohgoumenou grammatos apo to A + thn diafora auth
@@ -22,7 +22,7 @@ for lexi in keimeno:##gia kathe lexi
         l.append(chr(ascii))##antikathistw ta palia grammata me ta kainourgia se ROT13 apothikeyontas ta se mia lista
     p = ""##dhmiourgia ths neas lexis
     for i in l:##kathe gramma ths listas
-        p +=i##prosthsese to sthn nea lexi 
+        p +=i##prosthsese to sthn nea lexi
     p +=" "##prosthese ena keno meta thn kathe lexi
     P +=p##prosthese sthn nea protash thn nea lexi me to keno sto telos
 print P##emfanizw thn kainourgia protash
---
+++ askhsh_6.py
@@ -0,0 +1,19 @@
+import datetime
+year = input("give me the year: ")
+month = input("give me the month: ")
+if (month == 1 or month == 3 or month == 5 or month == 7 or month == 8 or month == 10 or month == 12):
+    stop = 31
+elif (month == 4 or month == 6 or month == 9 or month == 11):
+    stop = 30
+else: #month == 2
+    if (year % 4 == 0 and year % 100 != 0):#elegxw an ena etos
+        stop = 29                         #einai disekto
+    elif (year % 400 == 0):                #opou tote tha exei 
+        stop = 29                         #29 hmeres
+    else:                                  #enw alliws tha exei 
+        stop = 28                         #28
+print stop
+for i in range (1,stop + 1):
+    d = datetime.date(year, month , i)
+    print d.isoweekday()
+
