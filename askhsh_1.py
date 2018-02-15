c99fc801d810f555d735717a9fd3ac01b3808af0
Markos Arakynthios <markos_ar@hotmail.com>
2018-02-15 21:52:25 +0200

    I changed the code...it's better now!

--- askhsh_1.py
+++ askhsh_1.py
@@ -1,56 +1,35 @@
-def search(r,m):##sequential search opws thn mathame sto lykeio(se python of course)
-    done = False # brethke to stoixeio;
-    position = -1 ## arxikopoiw thn thesi tou stoixeiou pou anazhtw sthn lista 
-    i = 0
-    while (i < len(m) and done == False):##na mhn bgw ektos oriwn ths listas kia elegxw pws den vrethike akomh to stoixeio
-        if(m[i]== r):
-            done = True
-            position = i
-        else:
-            i += 1
-    return position        
-
-
 import random
 s = 0 ## to s einai to athroisma twn forwn pou trexei to paixnidi mexri kapoios na pei "bingo"
 for game in range(1000):
-    l = []
-    N = 5
+    l = []              #dhmiourgw mia lista
     for i in range(100):
-        l.append([])
-        for j in range(N):
+        l.append([])#pou tha periexei tis 100 listes opou kathe mia tha periexei tous 5 arithmous twn 100 paiktwn
+        j = 0
+        while (j < 5):
             z = random.randint(1,80)
-            ##parakatw einai o xeirokinhtos tropos pou den einai kai toso praktikos
-            ##z = int(input('Choose a number(from 1 to 80): '))
-            ##while(z < 1 or z > 80):
-                ##print "Wrong input.Give again..."
-                ##z = int(input('Choose a number(from 1 to 80): '))
-            l[i].append(z)
+            if z not in l[i]:#elegxw pws den exw xana valei ton arithmo z sthn ekastote lista mou
+                l[i].append(z)
+                j+=1         
     print "h lista me tous arithmous twn paixtwn einai: "
     print l
     m = []
     for i in range(1,81):
-         m.append(i)
-    bingo = False #elegxw an eipe kaneis bingo
+        m.append(i)#dhmiourgia lista m me tous arithmous 1,2,3,...,80
+    bingo = False #elegxos gia to an eipe kaneis bingo
     fores = 0 ##poses fores trexei to paixnidi 
     while(bingo == False and len(m) > 0):
-        r = random.choice(m)
-        print r # ektypwnw tonarithmo poy "epelexe" to pc
-        p = search(r,m)
-        if (p!= -1):
-            del m[p]
+        r = random.choice(m)#tuxaia epilogh arithmou apo thn lista m
+        print r # ektypwnw ton arithmo poy "epelexe" to pc
+        p = m.index(r)#pou einai to r 
+        del m[p]#diagrafh tou r apo thn lista m
         print m #ektypwnw thn lista tou pc meta thn afairesh toy prohgoymenoy arithmoy
         for i in l:
-            p1 = search(r,i)
-            if (p1 != -1):
-                del i[p1]
+            if r in i:#an uparxei to r sthn ekastote lista
+                p1 = i.index(r)#vres thn thesi tou
+                del i[p1]#kai diegrapse to  
             if (i == []):## an kapoia lista twn paixtwn einai kenh..."bingo"
                 bingo = True
-            ## apenergopihste to sxolio apo thn epomenh grammh gia na deite tis listes twn paixtwn se kathe "gyro" tou paixnidiou
-            ##print i
         fores += 1
     s += fores
-print s/1000 ##emfanizw to meso oro 
-        
-            
-                
+print s/1000 ##emfanizw to meso oro
+
